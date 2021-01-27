d8ff1d8333182472d1e58143aa7fe3e6f0cda7cc
Hitarth <hitarthchudgar@gmail.com>
2021-01-27 16:59:12 -0500

    Slider works now

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const validHex = (hex) => {
 //modifying inputColor on the box div dynamically
 const hexIn = document.getElementById('hexIn');
 const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('outputColor');
+const alteredColorText = document.getElementById('outputColorText')
 
 hexIn.addEventListener('keyup', () => {
     const hex = hexIn.value; //whatever is inputted by the user
@@ -21,7 +23,7 @@ hexIn.addEventListener('keyup', () => {
 
 //converting hex to rgb
 const convertHexToRGB = (hex) => {
-    if (!ValidHex(hex)) return null;
+    if (!validHex(hex)) return null;
 
     let strippedHex = hex.replace('#', '');
 
@@ -58,5 +60,32 @@ const slider = document.getElementById('slider');
 const sliderText = document.getElementById('sliderText');
 
 slider.addEventListener('input', () => {
-  sliderText.textContent = `${slider.value}%`;
+    if(!validHex(hexIn.value)) return;
+    sliderText.textContent = `${slider.value}%`;
+    //get the altered hex value
+    const alteredHex = alterColor(hexIn.value, slider.value);
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`;
 })
+
+//make sure it doesn't cross 255
+const increaseWithin0To255 = (hex, amount) => {
+    return Math.min(255, Math.max(0, hex + amount));
+}
+
+const alterColor = (hex, percentage) => {
+    const {
+        r,
+        g,
+        b
+    } = convertHexToRGB(hex);
+    const amount = Math.floor((percentage / 100) * 255); //preventing decimal
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount)
+    console.log(newR, newG, newB);
+    return convertRGBtoHex(newR, newG, newB);
+}
+
+console.log(alterColor('000', 10)); //testing the amount reduction
+
